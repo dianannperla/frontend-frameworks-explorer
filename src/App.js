db70656d96e74c0fce9b9d772aa551553053ebdb
dianannperla <waafirmelyza@hotmail.com>
2025-09-26 12:41:40 +0100

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Chào mừng đến với Frontend Frameworks Explorer</h1>
+      <p>Khám phá và so sánh các frontend frameworks!</p>
+    </div>
+  );
+}
+
+export default App;
